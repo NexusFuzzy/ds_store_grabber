fe198dbf3aaa3396c2a0d283617f446be28b0925
NexusFuzzy <mario.henkel4@gmail.com>
2025-06-18 08:29:49 +0200

    Update ds_store_grabber.py

--- ds_store_grabber.py
+++ ds_store_grabber.py
@@ -5,11 +5,16 @@ import urllib3
 import base64
 from datetime import datetime
 import os
+import shutil
+import fnmatch
 
 urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
 
 found_urls = []
 base_url = ""
+
+DS_Store_Entries = []
+
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -25,6 +30,10 @@ def str2bool(v):
 def scan(url, download, output_path, base_url):
     try:
         url_b64 = base64.b64encode(url.encode()).decode()
+        
+        if not os.path.exists(output_path + "/DS_Store_Files"):
+            os.makedirs(output_path + "/DS_Store_Files", exist_ok=True)
+            
         print("Trying to download " + url + "/.DS_Store")
         r = requests.get(url + "/.DS_Store", verify=False, stream=True)
         r.raw.decode_content = True
@@ -34,10 +43,12 @@ def scan(url, download, output_path, base_url):
             os.makedirs(output_folder, exist_ok=True)
 
         if r.status_code == 200:
-            with open(url_b64 + "_DS_Store", "wb") as f:
+            with open(output_path + "/" + url_b64 + "_DS_Store", "wb") as f:
                 for chunk in r.iter_content(chunk_size=1024):
                     f.write(chunk)
-            with DSStore.open(url_b64 + "_DS_Store", "r") as d:
+            with DSStore.open(output_path + "/" + url_b64 + "_DS_Store", "r") as d:
+                # If it's a valid .DS_Store file we want to keep it separate
+                shutil.copyfile(output_path + "/" + url_b64 + "_DS_Store", output_path + "/DS_Store_Files/" + url_b64 + "_DS_Store")
                 for entry in d:
                     if entry.code.decode("utf-8") == "Iloc":
                         print("Found possible file or folder: " + entry.filename)
@@ -45,6 +56,7 @@ def scan(url, download, output_path, base_url):
                         
                         if download:
                             try:
+                               # Download the possible file
                                r_file = requests.get(url + "/" + entry.filename, verify=False, stream=True)
                                r_file.raw.decode_content = True
                                if r_file.status_code==200:
@@ -57,7 +69,6 @@ def scan(url, download, output_path, base_url):
                             except:
                                print("Couldn't download!")
                         scan(url + "/" + entry.filename, download, output_path, base_url)
-            os.remove(url_b64 + "_DS_Store")
                         
         else:
             print("Got unexpected HTTP code " + str(r.status_code))
@@ -80,9 +91,20 @@ def main():
         os.makedirs(output_path, exist_ok=True)
         
     scan(args.url, args.download, output_path, args.url)
-    print("RESULTS:\n")
+    
+    # Cleaning up
+    pattern = "_DS_Store"
+    for file_name in os.listdir(output_path):
+        if file_name.endswith(pattern):
+            file_path = os.path.join(output_path, file_name)
+            os.remove(file_path)        
+
+    print("Extracted data:\n")
     for url in found_urls:
         print(url)
+        
+    if args.download:
+        print("All files have been written to " + output_path)
 
 if __name__ == "__main__":
     main()
